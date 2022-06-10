537df817b11e516d0b4a84a08b085ca2f0b9a115
Genrev Linao <gflinao@iskolarngbayan.pup.edu.ph>
2022-06-10 17:45:19 +0800

    Design changes / Done with the Contact Tracing Form!

--- Contact-Tracing-VB/Form1.Designer.vb
+++ Contact-Tracing-VB/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.TxtbxFN = New System.Windows.Forms.TextBox()
         Me.TxtbxAge = New System.Windows.Forms.TextBox()
         Me.TxtbxMN = New System.Windows.Forms.TextBox()
@@ -44,10 +45,12 @@ Partial Class Form1
         Me.CbLoss = New System.Windows.Forms.CheckBox()
         Me.CbNone = New System.Windows.Forms.CheckBox()
         Me.BtnSub = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'TxtbxFN
         '
+        Me.TxtbxFN.Font = New System.Drawing.Font("Consolas", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TxtbxFN.Location = New System.Drawing.Point(12, 84)
         Me.TxtbxFN.Multiline = True
         Me.TxtbxFN.Name = "TxtbxFN"
@@ -56,6 +59,7 @@ Partial Class Form1
         '
         'TxtbxAge
         '
+        Me.TxtbxAge.Font = New System.Drawing.Font("Consolas", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TxtbxAge.Location = New System.Drawing.Point(12, 143)
         Me.TxtbxAge.Multiline = True
         Me.TxtbxAge.Name = "TxtbxAge"
@@ -64,6 +68,7 @@ Partial Class Form1
         '
         'TxtbxMN
         '
+        Me.TxtbxMN.Font = New System.Drawing.Font("Consolas", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TxtbxMN.Location = New System.Drawing.Point(92, 143)
         Me.TxtbxMN.Multiline = True
         Me.TxtbxMN.Name = "TxtbxMN"
@@ -72,6 +77,7 @@ Partial Class Form1
         '
         'TxtbxAdd
         '
+        Me.TxtbxAdd.Font = New System.Drawing.Font("Consolas", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TxtbxAdd.Location = New System.Drawing.Point(12, 263)
         Me.TxtbxAdd.Multiline = True
         Me.TxtbxAdd.Name = "TxtbxAdd"
@@ -80,6 +86,7 @@ Partial Class Form1
         '
         'TxtbxEAdd
         '
+        Me.TxtbxEAdd.Font = New System.Drawing.Font("Consolas", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TxtbxEAdd.Location = New System.Drawing.Point(12, 204)
         Me.TxtbxEAdd.Multiline = True
         Me.TxtbxEAdd.Name = "TxtbxEAdd"
@@ -89,7 +96,9 @@ Partial Class Form1
         'LblFN
         '
         Me.LblFN.AutoSize = True
+        Me.LblFN.BackColor = System.Drawing.Color.Transparent
         Me.LblFN.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblFN.ForeColor = System.Drawing.Color.Black
         Me.LblFN.Location = New System.Drawing.Point(8, 62)
         Me.LblFN.Name = "LblFN"
         Me.LblFN.Size = New System.Drawing.Size(99, 19)
@@ -99,6 +108,7 @@ Partial Class Form1
         'LblAge
         '
         Me.LblAge.AutoSize = True
+        Me.LblAge.BackColor = System.Drawing.Color.Transparent
         Me.LblAge.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.LblAge.Location = New System.Drawing.Point(8, 121)
         Me.LblAge.Name = "LblAge"
@@ -109,6 +119,7 @@ Partial Class Form1
         'LblMN
         '
         Me.LblMN.AutoSize = True
+        Me.LblMN.BackColor = System.Drawing.Color.Transparent
         Me.LblMN.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.LblMN.Location = New System.Drawing.Point(88, 121)
         Me.LblMN.Name = "LblMN"
@@ -119,6 +130,7 @@ Partial Class Form1
         'LblEA
         '
         Me.LblEA.AutoSize = True
+        Me.LblEA.BackColor = System.Drawing.Color.Transparent
         Me.LblEA.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.LblEA.Location = New System.Drawing.Point(8, 180)
         Me.LblEA.Name = "LblEA"
@@ -129,6 +141,7 @@ Partial Class Form1
         'LblAdd
         '
         Me.LblAdd.AutoSize = True
+        Me.LblAdd.BackColor = System.Drawing.Color.Transparent
         Me.LblAdd.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.LblAdd.Location = New System.Drawing.Point(8, 241)
         Me.LblAdd.Name = "LblAdd"
@@ -139,6 +152,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
         Me.Label1.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.Location = New System.Drawing.Point(9, 305)
         Me.Label1.Name = "Label1"
@@ -149,28 +163,31 @@ Partial Class Form1
         'CbY1
         '
         Me.CbY1.AutoSize = True
+        Me.CbY1.BackColor = System.Drawing.Color.Transparent
         Me.CbY1.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CbY1.Location = New System.Drawing.Point(12, 325)
         Me.CbY1.Name = "CbY1"
         Me.CbY1.Size = New System.Drawing.Size(44, 17)
         Me.CbY1.TabIndex = 11
         Me.CbY1.Text = "Yes"
-        Me.CbY1.UseVisualStyleBackColor = True
+        Me.CbY1.UseVisualStyleBackColor = False
         '
         'CbN1
         '
         Me.CbN1.AutoSize = True
+        Me.CbN1.BackColor = System.Drawing.Color.Transparent
         Me.CbN1.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CbN1.Location = New System.Drawing.Point(62, 325)
         Me.CbN1.Name = "CbN1"
         Me.CbN1.Size = New System.Drawing.Size(38, 17)
         Me.CbN1.TabIndex = 12
         Me.CbN1.Text = "No"
-        Me.CbN1.UseVisualStyleBackColor = True
+        Me.CbN1.UseVisualStyleBackColor = False
         '
         'Label2
         '
         Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
         Me.Label2.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.Location = New System.Drawing.Point(9, 348)
         Me.Label2.Name = "Label2"
@@ -181,28 +198,31 @@ Partial Class Form1
         'CbY2
         '
         Me.CbY2.AutoSize = True
+        Me.CbY2.BackColor = System.Drawing.Color.Transparent
         Me.CbY2.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CbY2.Location = New System.Drawing.Point(12, 368)
         Me.CbY2.Name = "CbY2"
         Me.CbY2.Size = New System.Drawing.Size(44, 17)
         Me.CbY2.TabIndex = 14
         Me.CbY2.Text = "Yes"
-        Me.CbY2.UseVisualStyleBackColor = True
+        Me.CbY2.UseVisualStyleBackColor = False
         '
         'CbN2
         '
         Me.CbN2.AutoSize = True
+        Me.CbN2.BackColor = System.Drawing.Color.Transparent
         Me.CbN2.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CbN2.Location = New System.Drawing.Point(62, 368)
         Me.CbN2.Name = "CbN2"
         Me.CbN2.Size = New System.Drawing.Size(38, 17)
         Me.CbN2.TabIndex = 15
         Me.CbN2.Text = "No"
-        Me.CbN2.UseVisualStyleBackColor = True
+        Me.CbN2.UseVisualStyleBackColor = False
         '
         'Label3
         '
         Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
         Me.Label3.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label3.Location = New System.Drawing.Point(9, 390)
         Me.Label3.Name = "Label3"
@@ -213,62 +233,82 @@ Partial Class Form1
         'CbCough
         '
         Me.CbCough.AutoSize = True
+        Me.CbCough.BackColor = System.Drawing.Color.Transparent
         Me.CbCough.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CbCough.Location = New System.Drawing.Point(9, 424)
         Me.CbCough.Name = "CbCough"
         Me.CbCough.Size = New System.Drawing.Size(56, 17)
         Me.CbCough.TabIndex = 17
         Me.CbCough.Text = "Cough"
-        Me.CbCough.UseVisualStyleBackColor = True
+        Me.CbCough.UseVisualStyleBackColor = False
         '
         'CbFever
         '
         Me.CbFever.AutoSize = True
+        Me.CbFever.BackColor = System.Drawing.Color.Transparent
         Me.CbFever.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CbFever.Location = New System.Drawing.Point(9, 447)
         Me.CbFever.Name = "CbFever"
         Me.CbFever.Size = New System.Drawing.Size(56, 17)
         Me.CbFever.TabIndex = 18
         Me.CbFever.Text = "Fever"
-        Me.CbFever.UseVisualStyleBackColor = True
+        Me.CbFever.UseVisualStyleBackColor = False
         '
         'CbLoss
         '
         Me.CbLoss.AutoSize = True
+        Me.CbLoss.BackColor = System.Drawing.Color.Transparent
         Me.CbLoss.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CbLoss.Location = New System.Drawing.Point(71, 424)
         Me.CbLoss.Name = "CbLoss"
         Me.CbLoss.Size = New System.Drawing.Size(158, 17)
         Me.CbLoss.TabIndex = 19
         Me.CbLoss.Text = "Loss of Smell or Taste"
-        Me.CbLoss.UseVisualStyleBackColor = True
+        Me.CbLoss.UseVisualStyleBackColor = False
         '
         'CbNone
         '
         Me.CbNone.AutoSize = True
+        Me.CbNone.BackColor = System.Drawing.Color.Transparent
         Me.CbNone.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CbNone.Location = New System.Drawing.Point(71, 447)
         Me.CbNone.Name = "CbNone"
         Me.CbNone.Size = New System.Drawing.Size(50, 17)
         Me.CbNone.TabIndex = 20
         Me.CbNone.Text = "None"
-        Me.CbNone.UseVisualStyleBackColor = True
+        Me.CbNone.UseVisualStyleBackColor = False
         '
         'BtnSub
         '
+        Me.BtnSub.BackColor = System.Drawing.Color.ForestGreen
         Me.BtnSub.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnSub.ForeColor = System.Drawing.Color.White
         Me.BtnSub.Location = New System.Drawing.Point(214, 466)
         Me.BtnSub.Name = "BtnSub"
         Me.BtnSub.Size = New System.Drawing.Size(99, 33)
         Me.BtnSub.TabIndex = 21
         Me.BtnSub.Text = "Submit"
-        Me.BtnSub.UseVisualStyleBackColor = True
+        Me.BtnSub.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Consolas", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.ForestGreen
+        Me.Label4.Location = New System.Drawing.Point(-1, 7)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(258, 56)
+        Me.Label4.TabIndex = 22
+        Me.Label4.Text = "Trace 2.0"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.ClientSize = New System.Drawing.Size(326, 509)
+        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.BtnSub)
         Me.Controls.Add(Me.CbNone)
         Me.Controls.Add(Me.CbLoss)
@@ -320,4 +360,5 @@ Partial Class Form1
     Friend WithEvents CbLoss As CheckBox
     Friend WithEvents CbNone As CheckBox
     Friend WithEvents BtnSub As Button
+    Friend WithEvents Label4 As Label
 End Class

cf563bc491d85d815667209947e85affbc29f891
Genrev Linao <gflinao@iskolarngbayan.pup.edu.ph>
2022-06-10 16:46:23 +0800

    Added Submit button / Name changes for Text boxes / Added function/Code for the Submit button

--- Contact-Tracing-VB/Form1.Designer.vb
+++ Contact-Tracing-VB/Form1.Designer.vb
@@ -22,23 +22,275 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TxtbxFN = New System.Windows.Forms.TextBox()
+        Me.TxtbxAge = New System.Windows.Forms.TextBox()
+        Me.TxtbxMN = New System.Windows.Forms.TextBox()
+        Me.TxtbxAdd = New System.Windows.Forms.TextBox()
+        Me.TxtbxEAdd = New System.Windows.Forms.TextBox()
+        Me.LblFN = New System.Windows.Forms.Label()
+        Me.LblAge = New System.Windows.Forms.Label()
+        Me.LblMN = New System.Windows.Forms.Label()
+        Me.LblEA = New System.Windows.Forms.Label()
+        Me.LblAdd = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CbY1 = New System.Windows.Forms.CheckBox()
+        Me.CbN1 = New System.Windows.Forms.CheckBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CbY2 = New System.Windows.Forms.CheckBox()
+        Me.CbN2 = New System.Windows.Forms.CheckBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.CbCough = New System.Windows.Forms.CheckBox()
+        Me.CbFever = New System.Windows.Forms.CheckBox()
+        Me.CbLoss = New System.Windows.Forms.CheckBox()
+        Me.CbNone = New System.Windows.Forms.CheckBox()
+        Me.BtnSub = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'TxtbxFN
         '
-        Me.TextBox1.Location = New System.Drawing.Point(126, 70)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 0
+        Me.TxtbxFN.Location = New System.Drawing.Point(12, 84)
+        Me.TxtbxFN.Multiline = True
+        Me.TxtbxFN.Name = "TxtbxFN"
+        Me.TxtbxFN.Size = New System.Drawing.Size(301, 26)
+        Me.TxtbxFN.TabIndex = 0
+        '
+        'TxtbxAge
+        '
+        Me.TxtbxAge.Location = New System.Drawing.Point(12, 143)
+        Me.TxtbxAge.Multiline = True
+        Me.TxtbxAge.Name = "TxtbxAge"
+        Me.TxtbxAge.Size = New System.Drawing.Size(63, 26)
+        Me.TxtbxAge.TabIndex = 1
+        '
+        'TxtbxMN
+        '
+        Me.TxtbxMN.Location = New System.Drawing.Point(92, 143)
+        Me.TxtbxMN.Multiline = True
+        Me.TxtbxMN.Name = "TxtbxMN"
+        Me.TxtbxMN.Size = New System.Drawing.Size(221, 26)
+        Me.TxtbxMN.TabIndex = 2
+        '
+        'TxtbxAdd
+        '
+        Me.TxtbxAdd.Location = New System.Drawing.Point(12, 263)
+        Me.TxtbxAdd.Multiline = True
+        Me.TxtbxAdd.Name = "TxtbxAdd"
+        Me.TxtbxAdd.Size = New System.Drawing.Size(301, 26)
+        Me.TxtbxAdd.TabIndex = 3
+        '
+        'TxtbxEAdd
+        '
+        Me.TxtbxEAdd.Location = New System.Drawing.Point(12, 204)
+        Me.TxtbxEAdd.Multiline = True
+        Me.TxtbxEAdd.Name = "TxtbxEAdd"
+        Me.TxtbxEAdd.Size = New System.Drawing.Size(301, 26)
+        Me.TxtbxEAdd.TabIndex = 4
+        '
+        'LblFN
+        '
+        Me.LblFN.AutoSize = True
+        Me.LblFN.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblFN.Location = New System.Drawing.Point(8, 62)
+        Me.LblFN.Name = "LblFN"
+        Me.LblFN.Size = New System.Drawing.Size(99, 19)
+        Me.LblFN.TabIndex = 5
+        Me.LblFN.Text = "Full Name:"
+        '
+        'LblAge
+        '
+        Me.LblAge.AutoSize = True
+        Me.LblAge.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblAge.Location = New System.Drawing.Point(8, 121)
+        Me.LblAge.Name = "LblAge"
+        Me.LblAge.Size = New System.Drawing.Size(45, 19)
+        Me.LblAge.TabIndex = 6
+        Me.LblAge.Text = "Age:"
+        '
+        'LblMN
+        '
+        Me.LblMN.AutoSize = True
+        Me.LblMN.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblMN.Location = New System.Drawing.Point(88, 121)
+        Me.LblMN.Name = "LblMN"
+        Me.LblMN.Size = New System.Drawing.Size(135, 19)
+        Me.LblMN.TabIndex = 7
+        Me.LblMN.Text = "Mobile Number:"
+        '
+        'LblEA
+        '
+        Me.LblEA.AutoSize = True
+        Me.LblEA.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblEA.Location = New System.Drawing.Point(8, 180)
+        Me.LblEA.Name = "LblEA"
+        Me.LblEA.Size = New System.Drawing.Size(135, 19)
+        Me.LblEA.TabIndex = 8
+        Me.LblEA.Text = "Email Address:"
+        '
+        'LblAdd
+        '
+        Me.LblAdd.AutoSize = True
+        Me.LblAdd.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblAdd.Location = New System.Drawing.Point(8, 241)
+        Me.LblAdd.Name = "LblAdd"
+        Me.LblAdd.Size = New System.Drawing.Size(81, 19)
+        Me.LblAdd.TabIndex = 9
+        Me.LblAdd.Text = "Address:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(9, 305)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(295, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "1.Have you traveled outside the country anytime?"
+        '
+        'CbY1
+        '
+        Me.CbY1.AutoSize = True
+        Me.CbY1.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CbY1.Location = New System.Drawing.Point(12, 325)
+        Me.CbY1.Name = "CbY1"
+        Me.CbY1.Size = New System.Drawing.Size(44, 17)
+        Me.CbY1.TabIndex = 11
+        Me.CbY1.Text = "Yes"
+        Me.CbY1.UseVisualStyleBackColor = True
+        '
+        'CbN1
+        '
+        Me.CbN1.AutoSize = True
+        Me.CbN1.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CbN1.Location = New System.Drawing.Point(62, 325)
+        Me.CbN1.Name = "CbN1"
+        Me.CbN1.Size = New System.Drawing.Size(38, 17)
+        Me.CbN1.TabIndex = 12
+        Me.CbN1.Text = "No"
+        Me.CbN1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(9, 348)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(169, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "2.Are you fully vaccinated?"
+        '
+        'CbY2
+        '
+        Me.CbY2.AutoSize = True
+        Me.CbY2.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CbY2.Location = New System.Drawing.Point(12, 368)
+        Me.CbY2.Name = "CbY2"
+        Me.CbY2.Size = New System.Drawing.Size(44, 17)
+        Me.CbY2.TabIndex = 14
+        Me.CbY2.Text = "Yes"
+        Me.CbY2.UseVisualStyleBackColor = True
+        '
+        'CbN2
+        '
+        Me.CbN2.AutoSize = True
+        Me.CbN2.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CbN2.Location = New System.Drawing.Point(62, 368)
+        Me.CbN2.Name = "CbN2"
+        Me.CbN2.Size = New System.Drawing.Size(38, 17)
+        Me.CbN2.TabIndex = 15
+        Me.CbN2.Text = "No"
+        Me.CbN2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(9, 390)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(271, 26)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "3.Do you currently have any of the following" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "conditions during this time:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'CbCough
+        '
+        Me.CbCough.AutoSize = True
+        Me.CbCough.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CbCough.Location = New System.Drawing.Point(9, 424)
+        Me.CbCough.Name = "CbCough"
+        Me.CbCough.Size = New System.Drawing.Size(56, 17)
+        Me.CbCough.TabIndex = 17
+        Me.CbCough.Text = "Cough"
+        Me.CbCough.UseVisualStyleBackColor = True
+        '
+        'CbFever
+        '
+        Me.CbFever.AutoSize = True
+        Me.CbFever.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CbFever.Location = New System.Drawing.Point(9, 447)
+        Me.CbFever.Name = "CbFever"
+        Me.CbFever.Size = New System.Drawing.Size(56, 17)
+        Me.CbFever.TabIndex = 18
+        Me.CbFever.Text = "Fever"
+        Me.CbFever.UseVisualStyleBackColor = True
+        '
+        'CbLoss
+        '
+        Me.CbLoss.AutoSize = True
+        Me.CbLoss.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CbLoss.Location = New System.Drawing.Point(71, 424)
+        Me.CbLoss.Name = "CbLoss"
+        Me.CbLoss.Size = New System.Drawing.Size(158, 17)
+        Me.CbLoss.TabIndex = 19
+        Me.CbLoss.Text = "Loss of Smell or Taste"
+        Me.CbLoss.UseVisualStyleBackColor = True
+        '
+        'CbNone
+        '
+        Me.CbNone.AutoSize = True
+        Me.CbNone.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CbNone.Location = New System.Drawing.Point(71, 447)
+        Me.CbNone.Name = "CbNone"
+        Me.CbNone.Size = New System.Drawing.Size(50, 17)
+        Me.CbNone.TabIndex = 20
+        Me.CbNone.Text = "None"
+        Me.CbNone.UseVisualStyleBackColor = True
+        '
+        'BtnSub
+        '
+        Me.BtnSub.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnSub.Location = New System.Drawing.Point(214, 466)
+        Me.BtnSub.Name = "BtnSub"
+        Me.BtnSub.Size = New System.Drawing.Size(99, 33)
+        Me.BtnSub.TabIndex = 21
+        Me.BtnSub.Text = "Submit"
+        Me.BtnSub.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(546, 348)
-        Me.Controls.Add(Me.TextBox1)
+        Me.ClientSize = New System.Drawing.Size(326, 509)
+        Me.Controls.Add(Me.BtnSub)
+        Me.Controls.Add(Me.CbNone)
+        Me.Controls.Add(Me.CbLoss)
+        Me.Controls.Add(Me.CbFever)
+        Me.Controls.Add(Me.CbCough)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.CbN2)
+        Me.Controls.Add(Me.CbY2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.CbN1)
+        Me.Controls.Add(Me.CbY1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LblAdd)
+        Me.Controls.Add(Me.LblEA)
+        Me.Controls.Add(Me.LblMN)
+        Me.Controls.Add(Me.LblAge)
+        Me.Controls.Add(Me.LblFN)
+        Me.Controls.Add(Me.TxtbxEAdd)
+        Me.Controls.Add(Me.TxtbxAdd)
+        Me.Controls.Add(Me.TxtbxMN)
+        Me.Controls.Add(Me.TxtbxAge)
+        Me.Controls.Add(Me.TxtbxFN)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -46,5 +298,26 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TxtbxFN As TextBox
+    Friend WithEvents TxtbxAge As TextBox
+    Friend WithEvents TxtbxMN As TextBox
+    Friend WithEvents TxtbxAdd As TextBox
+    Friend WithEvents TxtbxEAdd As TextBox
+    Friend WithEvents LblFN As Label
+    Friend WithEvents LblAge As Label
+    Friend WithEvents LblMN As Label
+    Friend WithEvents LblEA As Label
+    Friend WithEvents LblAdd As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CbY1 As CheckBox
+    Friend WithEvents CbN1 As CheckBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CbY2 As CheckBox
+    Friend WithEvents CbN2 As CheckBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents CbCough As CheckBox
+    Friend WithEvents CbFever As CheckBox
+    Friend WithEvents CbLoss As CheckBox
+    Friend WithEvents CbNone As CheckBox
+    Friend WithEvents BtnSub As Button
 End Class
--- Contact-Tracing-VB/Form1.vb
+++ Contact-Tracing-VB/Form1.vb
@@ -1,3 +1,79 @@
 ﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TxtbxFN.TextChanged
 
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TxtbxAdd.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TxtbxAge.TextChanged
+
+    End Sub
+
+    Private Sub LblAdd_Click(sender As Object, e As EventArgs) Handles LblAdd.Click
+
+    End Sub
+
+    Private Sub LblMN_Click(sender As Object, e As EventArgs) Handles LblMN.Click
+
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CbN1.CheckedChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub BtnSub_Click(sender As Object, e As EventArgs) Handles BtnSub.Click
+        Dim cform As System.IO.StreamWriter
+        cform = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\admin\source\repos\Contact-Tracing-VB\TracingForm.txt", True)
+        cform.WriteLine("Full Name: " + TxtbxFN.Text)
+        cform.WriteLine("Age: " + TxtbxAge.Text)
+        cform.WriteLine("Mobile Number: " + TxtbxMN.Text)
+        cform.WriteLine("Email Address: " + TxtbxEAdd.Text)
+        cform.WriteLine("Address: " + TxtbxAdd.Text)
+        cform.WriteLine("")
+
+        If CbY1.CheckState = CheckState.Checked Then
+            cform.WriteLine("1.Have you traveled outside the country anytime?: Yes")
+        Else
+            cform.WriteLine("1.Have you traveled outside the country anytime?: No")
+        End If
+
+        If CbY2.CheckState = CheckState.Checked Then
+            cform.WriteLine("2.Are you fully vaccinated?: Yes")
+        Else
+            cform.WriteLine("2.Are you fully vaccinated?: No")
+        End If
+
+        If CbCough.CheckState = CheckState.Checked Then
+            cform.WriteLine("3.Do you currently have any of the following conditions during this time: Cough")
+        Else
+            'Do Nothing
+        End If
+
+        If CbFever.CheckState = CheckState.Checked Then
+            cform.WriteLine("3.Do you currently have any of the following conditions during this time: Fever")
+        Else
+            'Do Nothing
+        End If
+
+        If CbLoss.CheckState = CheckState.Checked Then
+            cform.WriteLine("3.Do you currently have any of the following conditions during this time: Loss of Smell or Taste")
+        Else
+            'Do Nothing
+        End If
+
+        If CbNone.CheckState = CheckState.Checked Then
+            cform.WriteLine("3.Do you currently have any of the following conditions during this time: None")
+        Else
+            'Do Nothing
+        End If
+
+        cform.Close()
+
+    End Sub
 End Class
